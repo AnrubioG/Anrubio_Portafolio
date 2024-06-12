d06914ec691e0257335eee18930f270253585913
AnrubioG <angela.rubio@genstudents.org>
2024-06-11 23:45:09 -0500

    se agregan portafolio y contacto

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // elementos del DOM
 const list = document.querySelectorAll(".list");
+const skills = document.querySelector("#sobre-mi");
 
 // Efecto de icono activo
 function activeLink() {
@@ -12,3 +13,19 @@ function activeLink() {
 list.forEach((item) => {
   item.addEventListener("click", activeLink);
 });
+
+// efecto en barra de skills
+
+window.onscroll = function () {
+  animacionSkills();
+};
+
+function animacionSkills() {
+  let posicionSkills = window.innerHeight - skills.getBoundingClientRect().top;
+  if (posicionSkills >= 400) {
+    document.querySelector("#js").className = "progreso js";
+    document.querySelector("#hl").className = "progreso hl";
+    document.querySelector("#cs").className = "progreso cs";
+    document.querySelector("#jv").className = "progreso jv";
+  }
+}
